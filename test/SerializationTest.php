16a7fd69ba803c2611371f8ee4a394e6ba3d1a3f
Dmytro Dzubenko <dmytro@dzubenko.name>
2017-03-14 20:26:42 +0200

    Update serialization test

--- test/SerializationTest.php
+++ test/SerializationTest.php
@@ -10,24 +10,76 @@ class SerializationTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_should_be_same_after_serialization()
+    public function it_allows_one_assignment_per_property()
     {
         $foo = new Foo();
         $foo->bar = 1;
         $foo->baz = 2;
 
-        $fooClone = clone $foo;
+        $newFoo = unserialize(serialize($foo));
 
-        $serializedValue = serialize($foo);
-        $deSerializedValue = unserialize($serializedValue);
+        $this->assertSame(1, $newFoo->bar);
+        $this->assertSame(2, $newFoo->baz);
+    }
 
-        $this->assertSame($deSerializedValue->bar, $foo->bar);
-        $this->assertSame($deSerializedValue->baz, $foo->baz);
+    /**
+     * @test
+     */
+    public function it_fails_when_a_property_gets_reassigned()
+    {
+        $foo = new Foo();
+        $foo->bar = 1;
+        $newFoo = unserialize(serialize($foo));
 
-        $this->assertSame($fooClone->bar, $foo->bar);
-        $this->assertSame($fooClone->baz, $foo->baz);
+        $this->assertSame(1, $newFoo->bar);
 
-        $this->assertSame($fooClone->bar, $foo->bar);
-        $this->assertSame($fooClone->baz, $foo->baz);
+        $this->setExpectedException(\LogicException::class);
+        $newFoo->bar = 2;
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_to_assign_values_to_undefined_properties()
+    {
+        $foo = new Foo();
+        $newFoo = unserialize(serialize($foo));
+
+        $this->setExpectedException(\LogicException::class);
+        $newFoo->bang = 1;
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_to_retrieve_values_from_undefined_properties()
+    {
+        $foo = new Foo();
+        $newFoo = unserialize(serialize($foo));
+
+        $this->setExpectedException(\LogicException::class);
+        $newFoo->bang;
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_for_properties_that_have_no_assigned_value()
+    {
+        $foo = new Foo();
+        $newFoo = unserialize(serialize($foo));
+
+        $this->assertSame(null, $newFoo->bar);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_the_default_attribute_value_of_properties_that_have_no_assigned_value()
+    {
+        $foo = new Foo();
+        $newFoo = unserialize(serialize($foo));
+
+        $this->assertSame([], $newFoo->bazzes);
     }
 }
