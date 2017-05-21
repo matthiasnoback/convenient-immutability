ce7f7be943bfe6f8d2d8ec976ea0dfd85163f458
Jacob Dreesen <jacob@hdreesen.de>
2017-05-21 14:54:43 +0200

    Fix typos in test method name

--- test/ImmutableTraitTest.php
+++ test/ImmutableTraitTest.php
@@ -95,7 +95,7 @@ class ImmutableTraitTest extends \PHPUnit_Framework_TestCase
      * @test
      * @dataProvider objectProvider
      */
-    public function is_should_have_the_same_values_after_deserealization(Foo $foo)
+    public function it_should_have_the_same_values_after_deserialization(Foo $foo)
     {
         $foo->bar = 'bar';
         $foo->baz = 1;
